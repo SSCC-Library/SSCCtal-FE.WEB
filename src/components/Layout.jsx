c1ca3b96ba2bad412d24372019357b122f397b3d
Jeong <andy037200@gmail.com>
2025-07-01 18:48:58 +0900

    design: layout 디자인 변경 (#2)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,15 @@
 import Nav from './nav';
 import { Outlet } from 'react-router-dom';
+import './layout.css';
 
 function Layout() {
 	return (
-		<>
+		<div className="layout-wrap">
 			<Nav />
-			<main>
+			<main className="main-container">
 				<Outlet />
 			</main>
-		</>
+		</div>
 	);
 }
 
