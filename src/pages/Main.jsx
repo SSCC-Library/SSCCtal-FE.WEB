5ebc3ce80311ce14b0464e50d02f45edd37e6dee
chaerishme <sbk93@naver.com>
2025-08-25 22:31:09 +0900

    style: 대여기간 1주로 안내 수정 (#1)

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
   return (
     <div className="notice-box">
       <div className="title">대여 이용 안내</div>
-      <p>기본 대여 기간은 <strong>2주</strong>입니다.</p>
+      <p>기본 대여 기간은 <strong>1주</strong>입니다.</p>
       <p>
         물품 대여는 동아리방 내 키오스크에서만 진행되며,<br />
         대여하지 않은 물품은 <strong>외부 반출이 불가능</strong>합니다.
